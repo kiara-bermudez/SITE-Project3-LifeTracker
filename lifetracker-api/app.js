8893e522944309fed2f77ef94ab59191ec42f0f0
kiara-bermudez <kiarab@tamu.edu>
2022-06-30 13:07:59 -0500

    Created basic server skeleton

---
+++ lifetracker-api/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const { NotFoundError} = require("./utils/errors")
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan('tiny'));
+
+// Health endpoint
+app.get("/",  (req, res, next) => {
+    res.status(200).json({ping: "pong"})
+})
+
+app.use((req, res, next) => {
+    return next(new NotFoundError());
+})
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message;
+    
+    return res.status(status).json({
+        error: {message, status}
+    })
+})
+
+module.exports = app;
